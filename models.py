3d48c991e40857cdfa1052e057d5e525e9bc5fff
Bhuvan1131 <123910097+Bhuvan1131@users.noreply.github.com>
2025-04-12 12:25:36 -0400

    Update models.py

--- models.py
+++ models.py
@@ -6,8 +6,8 @@ class OnboardingEntry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     token = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(120), nullable=False)
-    joining_date = db.Column(db.String(20))
-
-    employee_info = db.Column(db.JSON)  # Stores all fields in one column
-    hr_info = db.Column(db.JSON)
-    status = db.Column(db.String(20))
+    joining_date = db.Column(db.String(20), nullable=False)
+    
+    employee_info = db.Column(db.JSON)  # stores entire employee section
+    hr_info = db.Column(db.JSON)        # stores entire HR section
+    status = db.Column(db.String(20), default='invited')
